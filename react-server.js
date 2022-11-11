3095f478b205a91b136191cc1dceb719499c2832
talib <imonn439@gmail.com>
2022-11-11 13:34:11 +0530

    new build uploaded

--- react-server.js
+++ react-server.js
@@ -2,34 +2,27 @@
 const path = require("path");
 const express = require("express");
 const app = express(); // create express app
-// add middleware
-// app.use(express.static("build2"));
-
-// app.use(express.static("build1"));
 
 console.log('before hitt')
+//home route will redirect to the citizen-pannel route
 app.get("/", (req, res) => {
  console.log('home route hitted')
- res.redirect('/citizen-pannel')
-});
-app.use(express.static("cp_build"));
-app.use(express.static("swm_build"));
-app.get("/citizen-pannel", (req, res) => {
-  res.sendFile(path.join(__dirname, "cp_build", "index.html"));
-});
-app.get("/swm", (req, res) => {
-  res.sendFile(path.join(__dirname, "swm_build", "index.html"));
+ res.redirect('/citizen')
 });
 
-// app.get('/', function (req, res) {
-//  res.redirect('/citizen-pannel')
-// })
-// app.get('*', function (req, res) {
-//  res.redirect('/citizen-pannel')
-// })
+//middleware to server static files also
+app.use(express.static("citizen_build"));
+app.use(express.static("admin_build"));
 
+//actual routes
+app.get("/citizen", (req, res) => {
+  res.sendFile(path.join(__dirname, "citizen_build", "index.html"));
+});
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "admin_build", "index.html"));
+});
 
-// start express server on port 5000
-app.listen(82, () => {
+// start express server on port 82
+app.listen(80, () => {
   console.log("server started on port 82");
 });
